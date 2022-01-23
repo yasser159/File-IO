d50e25f2eb9a4551ea352465287e3f13458171bd
Yasser Hajlaoui <yasser159@gmail.com>
2022-01-22 22:20:47 -0800

    Initial Commit

---
+++ File IO/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  File IO
+//
+//  Created by Yasser Hajlaoui on 1/22/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
