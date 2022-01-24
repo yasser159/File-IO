ce58e087820be46f25880793f519578395093771
Yasser Hajlaoui <yasser159@gmail.com>
2022-01-24 15:40:17 -0800

    finished

--- File IO/ViewController.swift
+++ File IO/ViewController.swift
@@ -1,57 +1,53 @@
 import Cocoa
 
 class ViewController: NSViewController {
-    @IBOutlet weak var txt_Input: NSTextFieldCell!
+    @IBOutlet weak var txt_fileName: NSTextFieldCell!
+    @IBOutlet weak var txt_txtContents: NSTextField!
     @IBOutlet weak var lbl_Output: NSTextFieldCell!
-
-    // Fine documents directory on device
-    let dirs : [String] = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.allDomainsMask, true)
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupFile()
-    }
-
-    // Set the contents
-    var fileContentToWrite = " "
+    var fileContentToWrite = ""
     let fileName = "myFileName.txt"
     var filePath = ""
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
     @IBAction func save(_ sender: Any) {
-        do {
-            // Write contents to file
-            try fileContentToWrite.write(toFile: filePath, atomically: false, encoding: String.Encoding.utf8)
-        }
-        catch let error as NSError {
-            print("An error took place: \(error)")
+        if checkInputes_Pass() {
+        saveTextFile(fileName_: fileName, text_:txt_fileName.stringValue)
         }
     }
       
-    @IBAction func Read(_ sender: Any) {  // Read file content. Example in Swift
-        do {
-            // Read file content
-            let contentFromFile = try NSString(contentsOfFile: filePath, encoding: String.Encoding.utf8.rawValue)
-            print(contentFromFile)
-            lbl_Output.stringValue = String(contentFromFile)
-        }
-        catch let error as NSError {
-            print("An error took place: \(error)")
-        }
-    }
-
-    func setupFile(){//__________________________________________________
-      if dirs.count > 0 {
-          let dir = dirs[0] //documents directory
-          filePath = dir.appending("/" + fileName)
-          //print("Local path = \(filePath)")
-      } else {
-          print("Could not find local directory to store file")
-          return
-      }
+    @IBAction func Read(_ sender: Any) {
+        lbl_Output.stringValue =  loadTextFile(fileName_: fileName)
     }
     
     override var representedObject: Any? {
         didSet {
         }
     }
+    
+    func checkInputes_Pass()->Bool{
+        var missingFields = [String]()
+        var NoMissingFields = true
+        
+        if txt_fileName.stringValue == "" {
+            missingFields.append("File Name")}
+            
+        if txt_txtContents.stringValue == ""{
+            missingFields.append("File Text")}
+
+        if missingFields.count > 0 {
+            NoMissingFields = false
+            var warning =  "Please Enter the missing Values: \n\r"
+            warning.append(missingFields.joined(separator: "\n\r"))
+            lbl_Output.stringValue = warning
+        } else {
+            lbl_Output.stringValue = ""
+        }
+        
+        return NoMissingFields
+    }
+    
 }//Last Bracket
