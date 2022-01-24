e21ab51b43f30c8659a512fca124cf73a43c7ae5
Yasser Hajlaoui <yasser159@gmail.com>
2022-01-24 00:02:18 -0800

    initial setup

--- File IO/ViewController.swift
+++ File IO/ViewController.swift
@@ -1,26 +1,63 @@
-//
-//  ViewController.swift
-//  File IO
-//
-//  Created by Yasser Hajlaoui on 1/22/22.
-//
-
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    @IBOutlet weak var txt_Input: NSTextFieldCell!
+    @IBOutlet weak var lbl_Output: NSTextFieldCell!
 
+    // Fine documents directory on device
+    let dirs : [String] = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.allDomainsMask, true)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupFile()
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    // Set the contents
+    let fileContentToWrite = "Text to be recorded into file"
+    let fileName = "myFileName.txt"
+    var filePath = ""
+    
+    @IBAction func save(_ sender: Any) {
+        do {
+            // Write contents to file
+            try fileContentToWrite.write(toFile: filePath, atomically: false, encoding: String.Encoding.utf8)
+        }
+        catch let error as NSError {
+            print("An error took place: \(error)")
+        }
+    }
+      
+    @IBAction func Read(_ sender: Any) {
+        // Read file content. Example in Swift
+        do {
+            // Read file content
+            let contentFromFile = try NSString(contentsOfFile: filePath, encoding: String.Encoding.utf8.rawValue)
+            print(contentFromFile)
+        }
+        catch let error as NSError {
+            print("An error took place: \(error)")
         }
     }
 
+    
 
-}
-
+    func setupFile(){//__________________________________________________
+      if dirs.count > 0 {
+          let dir = dirs[0] //documents directory
+          filePath = dir.appending("/" + fileName)
+          print("Local path = \(filePath)")
+      } else {
+          print("Could not find local directory to store file")
+          return
+      }
+    }
+    
+    
+    
+    
+    override var representedObject: Any? {
+        didSet {
+        }
+    }
+}//Last Bracket
